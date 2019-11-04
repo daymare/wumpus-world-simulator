5c167b245ad261e7c439f346b32c3314fc6e603a
daymare <daylankelting@gmail.com>
2019-11-04 08:33:09 -0800

    added run script for more convenient testing

---
+++ run.bash
@@ -0,0 +1,11 @@
+
+
+
+#pywumpsim -tries 10 -world ./testworlds/test_pit4.txt
+#pywumpsim -tries 10 -world ./testworlds/testworld.txt
+#pywumpsim -tries 10 -world ./testworlds/test_shoot_wumpus_basic.txt
+#pywumpsim -tries 10 -world ./testworlds/test_ignore_wumpus.txt
+#pywumpsim -tries 10 -world ./testworlds/test_shoot_wumpus_colocated.txt
+pywumpsim -tries 10 -world ./testworlds/bug1.txt
+
+#pywumpsim -tries 2 -trials 1000
